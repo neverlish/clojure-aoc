fb320f288f0cae262fe5879ae5f6fe24aaa6e8ab
neverlish <neverlish@gmail.com>
2021-12-07 15:30:14 +0900

    solved 2018 day1 - part2

---
+++ src/aoc_2018_01_02.clj
@@ -0,0 +1,52 @@
+; solves https://adventofcode.com/2018/day/1#part2
+
+(ns aoc_2018_01_02
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn get-file
+  [filename]
+  (->> filename
+       (str "file/")
+       (io/resource)
+       (slurp))
+  )
+
+(defn parse-int [s]
+  (Integer. (re-find  #"\d+" s )))
+
+(defn string-to-integer
+  [string]
+  (if (= (first string) \-)
+    (* (parse-int (subs string 1)) -1)
+    (parse-int (subs string 1))
+    )
+  )
+
+(defn split
+  [strings]
+  (str/split strings #"(\n)|(, )")
+  )
+
+(defn last-element-plus-nth-integer
+  [elements integers index]
+  (+ (or (last elements) 0) (nth integers index) ))
+
+(defn accumulation-reached-again-first
+  [accumulations integers index]
+  (let [value-added (last-element-plus-nth-integer accumulations integers index)]
+    (if (.contains accumulations value-added)
+      value-added
+      (recur
+        (conj accumulations value-added)
+        integers
+        (rem (+ index 1) (count integers)))
+      )
+    )
+  )
+
+(comment
+  (->> (get-file "2018_01_input.txt")
+       (split)
+       (map string-to-integer)
+       (#(accumulation-reached-again-first [] % 0))))
