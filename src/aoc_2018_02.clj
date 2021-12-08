061065c09c52710f5318acc202d1259745c80b30
neverlish <neverlish@gmail.com>
2021-12-08 13:59:19 +0900

    solved 2018 day2 - part2

--- src/aoc_2018_02.clj
+++ src/aoc_2018_02.clj
@@ -27,10 +27,52 @@
        (vals)
        (reduce *)))
 
+(defn common-chars-compare
+  "두 문자열의 벡터를 받아, 같은 위치의 문자가 같은 문자들을 반환한다."
+  [chars1 chars2]
+  (reduce-kv
+    (fn [result index char]
+      (if (= char (nth chars2 index))
+         (str result char)
+         result))
+    ""
+    chars1))
+
+(defn split-into-chars
+  [word]
+  (clojure.string/split word #""))
+
+(defn get-longer
+  [a b]
+  (if (> (count a) (count b)) a b))
+
+(defn most-common-char-in-row
+  "문자열 하나와, 문자열이 든 vector을 입력받는다.
+  vector 내에서 문자열과 가장 흡사한 값의 공통 문자들을 반환한다."
+  [comparer rows]
+  (reduce
+    (fn [result word]
+      (let [compared (common-chars-compare (split-into-chars comparer) (split-into-chars word))]
+        (get-longer compared result)))
+    ""
+    rows))
+
+(defn most-common-chars-in-rows
+  "문자열의 배열을 받아, 문자열간 가장 흡사한 문자들을 반환한다."
+  [rows]
+  (reduce-kv
+    (fn [result index row]
+      (let [common-in-row (most-common-char-in-row row (subvec rows (+ index 1)))]
+        (get-longer common-in-row result)))
+    ""
+    rows))
+
 (comment
   (->> (util/get-file "2018_02_input.txt")
        (clojure.string/split-lines)
        (map chars-occured-2_3-counted)
        (flatten)
-       (frequencies-multiplied)))
-
+       (frequencies-multiplied))
+  (->> (util/get-file "2018_02_input.txt")
+       (clojure.string/split-lines)
+       (most-common-chars-in-rows)))
