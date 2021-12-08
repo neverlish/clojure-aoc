5a2d888fb9e3131aee623d0da1bb2e930b323ea4
neverlish <neverlish@gmail.com>
2021-12-08 11:02:29 +0900

    added comment

--- src/aoc_2018_01.clj
+++ src/aoc_2018_01.clj
@@ -3,14 +3,22 @@
 (ns aoc_2018_01)
 
 (defn parse-int
+  "입력된 문자를 숫자로 반환한다."
   [string]
   (Integer/parseInt string))
 
 (defn peek-element
+  "vector를 입력하면 마지막 요소를 반환한다. vector가 빈 경우 0을 반환한다."
   [elements]
   (or (peek elements) 0))
 
 (defn accumulation-reached-again-first
+  "integers의 누적합 중 재출현하는 첫번째 값을 계산한다.
+  integers를 끝까지 누적해도 재출현하는 누적값이 없다면 기존의 누적합에 이어서 integers 의 첫번째요소부터 다시 누적한다.
+  입력형태:
+    accumulations: vector
+    index: int
+    integers: vector"
   [accumulations index integers]
   (let [value-added (+ (peek-element accumulations) (nth integers index))]
     (if (some #{value-added} accumulations)
@@ -20,13 +28,13 @@
         (rem (+ index 1) (count integers))
         integers))))
 
-
-
 (comment
+  ; day1 첫번째 파트 해결
   (->> (util/get-file "2018_01_input.txt")
        (clojure.string/split-lines)
        (map parse-int)
        (reduce +))
+  ; day1 두번째 파트 해결
   (->> (util/get-file "2018_01_input.txt")
        (clojure.string/split-lines)
        (map parse-int)
