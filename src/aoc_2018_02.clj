8ace64724b245716b23439cce5399e204ee12b23
neverlish <neverlish@gmail.com>
2021-12-09 15:13:58 +0900

    most-common-chars-in-rows 코드 개선

--- src/aoc_2018_02.clj
+++ src/aoc_2018_02.clj
@@ -31,35 +31,21 @@
 (defn common-chars-compare
   "두 문자열의 벡터를 받아, 같은 위치의 문자가 같은 문자들을 반환한다."
   [chars1 chars2]
-  (->> (map vector chars1 chars2)
-       (filter (fn [[a b]] (= a b)))
-       (map first)
-       (apply str)))
-
-(defn get-longer
-  [a b]
-  (if (> (count a) (count b)) a b))
-
-(defn most-common-char-in-row
-  "문자열 하나와, 문자열이 든 vector을 입력받는다.
-  vector 내에서 문자열과 가장 흡사한 값의 공통 문자들을 반환한다."
-  [comparer rows]
-  (reduce
-    (fn [result word]
-      (let [compared (common-chars-compare comparer word)]
-        (get-longer compared result)))
+  (if (= chars1 chars2)
     ""
-    rows))
+    (->> (map vector chars1 chars2)
+         (filter (fn [[a b]] (= a b)))
+         (map first)
+         (apply str))))
 
 (defn most-common-chars-in-rows
   "문자열의 배열을 받아, 문자열간 가장 흡사한 문자들을 반환한다."
   [rows]
-  (reduce-kv
-    (fn [result index row]
-      (let [common-in-row (most-common-char-in-row row (subvec rows (+ index 1)))]
-        (get-longer common-in-row result)))
-    ""
-    rows))
+  (->> (for [row1 rows row2 rows]
+         (common-chars-compare row1 row2))
+       (sort-by count)
+       last))
+
 
 (comment
   (->> (util/get-file "2018_02_input.txt")
