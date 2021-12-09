a2ee0e5e94f9d9dc5f67dbd9fbee9a0ddff2bbf8
neverlish <neverlish@gmail.com>
2021-12-09 16:44:28 +0900

    row-to-coordinte 에서 미리 area를 계산

--- src/aoc_2018_03.clj
+++ src/aoc_2018_03.clj
@@ -6,13 +6,14 @@
   "int 의 vector가 입력되면, 의미를 가지는 hash-map으로 변환한다.
   주의: end-x 와 end-y 는 exclusive 하다.
   입력예시: [1 1 3 4 4]
-  출력예시: {:id 1 :start-x 1 :start-y 3 :end-x 5 :end-y 7}"
+  출력예시: {:id 1 :start-x 1 :start-y 3 :end-x 5 :end-y 7 :area 16}"
   [[id x1 y1 x2 y2]]
   {:id      id
    :start-x x1
    :start-y y1
    :end-x   (+ x1 x2)
-   :end-y   (+ y1 y2)})
+   :end-y   (+ y1 y2)
+   :area (* x2 y2)})
 
 (defn row-to-coordinate
   "#1 @ 1,3: 4x4 와 같은 좌표정보가 들어오면, 좌표정보 좌상단 점과 우하단 점을 확인할 수 있는 hash-map을 반환한다.
@@ -64,14 +65,6 @@
            (filter #(coordinate-within-square? x y %))
            format))))
 
-(defn area
-  "사각형 정보를 받으면, 면적 정보를 추가하여 반환한다.
-  입력예시: {:start-x 1, :start-y 3, :end-x 5, :end-y 7}
-  출력예시: 16"
-  [square]
-  (* (- (square :end-x) (square :start-x))
-     (- (square :end-y) (square :start-y))))
-
 (defn squares-id-not-overlapped-frequencies
   "사각형 배열의 정보를 입력받으면, 겹치지 않는 사각형들의 좌표별 id 출현횟수를 반환한다.
   입력예시: {:entire {:x 7 :y 7} :squares '({:id 1 :start-x 1, :start-y 3, :end-x 5, :end-y 7} {:id 2 :start-x 3, :start-y 1, :end-x 7, :end-y 5})}
@@ -86,7 +79,7 @@
   [squares-and-entire]
   (let [freqs (squares-id-not-overlapped-frequencies squares-and-entire)]
     (->> (squares-and-entire :squares)
-         (filter (fn [square] (= (area square) (get freqs (square :id))))))))
+         (filter (fn [{:keys [area id]}] (= area (get freqs id)))))))
 
 (comment
   (->> "2018_03_input.txt"
