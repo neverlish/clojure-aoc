87aec8b07f4dbb8148dfa18e72be90bee2b39718
neverlish <neverlish@gmail.com>
2021-12-13 14:26:56 +0900

    2018 5일차 파트2 풀이

--- src/aoc_2018_05.clj
+++ src/aoc_2018_05.clj
@@ -36,7 +36,29 @@
     (recur to-deletes (s/replace target to-remove ""))
     target))
 
+(defn string-char-removed
+  "문자열과 지울값을 입력하면, 지울값의 대소문자 모두를 문자열에서 지운 값을 반환한다.
+
+  입력예시: qwEeaAb e
+  출력예시: qwaAb"
+  [string char]
+  (-> (s/replace string (s/lower-case char) "")
+      (s/replace (s/upper-case char) "")))
+
+(defn counted-per-char-removed-adjacent-insentively-same
+  "입력받은 문자열에서 알파벳을 하나씩 빼고 근접한 두 문자가 같은 알파벳을 제거한 후의 길이가 가장 짧은 경우를 반환한다."
+  [string]
+  (let [comparator (char-couples)]
+    (->> comparator
+         (map #(->> (string-char-removed string (first %))
+                    (removed-adjacent-insentively-same comparator)
+                    count))
+         sort)))
+
 (comment
   (->> (parsed-input)
        (#(removed-adjacent-insentively-same (char-couples) %))
-       count))
+       count)
+  (->> (parsed-input)
+       counted-per-char-removed-adjacent-insentively-same
+       first))
