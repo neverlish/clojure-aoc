683de9d7e4d9bb522b9ba01d918e15a88ac927aa
neverlish <neverlish@gmail.com>
2021-12-17 16:53:50 +0900

    for when에서 합의 2020여부 검사

--- src/aoc_2020_01.clj
+++ src/aoc_2020_01.clj
@@ -11,28 +11,23 @@
   [ints]
   (for [v1 ints
         v2 ints
-        :when (not= v1 v2)]
+        :when (and (not= v1 v2) (= 2020 (+ v1 v2)))]
     [v1 v2]))
 
 (defn vectors-int-diff-3d
   [ints]
   (for [v1 ints
-        [v2 v3] (vectors-int-diff-2d ints)
-        :when (and (not= v1 v2) (not= v1 v3))]
+        v2 ints
+        v3 ints
+        :when (and (not= v1 v2 v3) (= 2020 (+ v1 v2 v3)))]
     [v1 v2 v3]))
-
-(defn row-summed-to-2020
-  [rows]
-  (->> rows
-       (filter #(= (apply + %) 2020))
-       first))
 
 (comment
   (->> (data)
        vectors-int-diff-2d
-       row-summed-to-2020
+       first
        (apply *))
   (->> (data)
        vectors-int-diff-3d
-       row-summed-to-2020
+       first
        (apply *)))
