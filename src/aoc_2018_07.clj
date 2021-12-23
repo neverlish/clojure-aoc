c6dcfb2f8d6f6d31539598b2fa73e839496bcd8b
neverlish <neverlish@gmail.com>
2021-12-24 00:00:41 +0900

    주석 추가

--- src/aoc_2018_07.clj
+++ src/aoc_2018_07.clj
@@ -55,6 +55,10 @@
          (map append-time))))
 
 (defn decrease-time-if-worked
+  "그래프와 문자열로 된 업무목록을 받아, 그래프가 업무목록에 포함될 경우에만 그래프의 타임을 줄여서 반환한다.
+
+  입력예시: {:self C, :prerequisites [] :time 63} [C D]
+  출력예시: {:self C, :prerequisites [] :time 62}"
   [worked-list graph]
   (let [worked-set (->> worked-list (map :self) set)]
     (if (worked-set (graph :self))
@@ -62,13 +66,6 @@
       graph)))
 
 (defn graph-removed-done-workers
-  [graph done-workers]
-  (let [time-done-workers-set (set (map :self done-workers))]
-    (->> graph
-         (remove (set done-workers))
-         (map #(assoc % :prerequisites (remove time-done-workers-set (% :prerequisites)))))))
-
-(defn done-graph
   "그래프목록과 삭제대상을 입력받아,
   그래프 목록에서 삭제대상 자체를 삭제하고, 그래프 내부의 :prerequisites에서도 삭제한다.
 
@@ -84,6 +81,30 @@
          {:self B, :prerequisites [A]}
          {:self D, :prerequisites [A]}
          {:self E, :prerequisites [B D F]})"
+  [graph done-workers]
+  (let [time-done-workers-set (set (map :self done-workers))]
+    (->> graph
+         (remove (set done-workers))
+         (map #(assoc % :prerequisites (remove time-done-workers-set (% :prerequisites)))))))
+
+(defn done-graph
+  "업무자 그래프 정보와, 완료된 업무 정보를 입력받아
+  1. 업무자 그래프가 완료된 업무에 포함된 경우 시간을 1 줄인다
+  2. 1의 결과 중에서, 시간이 0이 된 것들을 뽑아 반환값의 :done에 포함한다.
+  3. 1의 결과 중 2의 결과에 포함되는 것들은 지워서 반환값의 :graph에 포함한다
+
+  입력예시: ({:self C, :prerequisites [] :time 1}
+           {:self A, :prerequisites [C] :time 2}
+           {:self F, :prerequisites [D] :time 2}
+           {:self B, :prerequisites [A] :time 3}
+           {:self D, :prerequisites [] :time 2}
+           {:self E, :prerequisites [B D F] :time 5}) [{:self C, :prerequisites []} {:self D, :prerequisites []}]
+
+  출력예시: ({:self A, :prerequisites [] :time 2}
+           {:self F, :prerequisites [D] :time 2}
+           {:self B, :prerequisites [A] :time 3}
+           {:self D, :prerequisites [] :time 1}
+           {:self E, :prerequisites [B D F] :time 5})"
   [workers done-list]
   (let [workers-passed-time (map #(decrease-time-if-worked done-list %) workers)
         done-workers (filter #(zero? (% :time)) workers-passed-time)
@@ -92,6 +113,8 @@
      :graph new-graph}))
 
 (defn workers-available
+  "그래프, 가능 업무자 수, 그동안의 업무이력을 입력받는다.
+  선결조건이 없는 업무들 중에서, 이전 업무와 이어져야 하거나 새로 시작 가능한 것들을 합쳐 가능 업무자수 만큼을 반환한다."
   [graphs worker-count worked-list]
   (let [last-worked (->> worked-list last (map :self) set)
         can-work (->> graphs
